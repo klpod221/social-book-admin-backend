18cf3ecda126ce435666037afff393da9ac45a86
klpod221 <klpod221@gmail.com>
2022-01-10 23:37:27 +0700

    update login api

--- src/main/java/com/socialbook/admin/controllers/AuthController.java
+++ src/main/java/com/socialbook/admin/controllers/AuthController.java
@@ -24,7 +24,7 @@ import com.socialbook.admin.security.services.UserDetailsImpl;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/admin/api/auth")
 public class AuthController {
     @Autowired
     AuthenticationManager authenticationManager;
